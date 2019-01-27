594c671bf92d856d48f58a846be40286182d6312
Eli Friedman <friedm3@cooper.edu>
2019-01-27 14:41:18 +0200

    fixing install things

--- ps1.py
+++ ps1.py
@@ -1,15 +1,10 @@
-#!/opt/bb/bin/python
+#!/usr/bin/python2
 import os
 s = os.getcwd()
 if len(s) > 30:
     s = ".." + s[-30:]
 #     s = '..'+s[s[:s.rfind('/')].rfind('/'):] # get last two folders
 
-
-if os.system("git rev-parse 2> /dev/null") == 0:
-    branch = os.popen("git rev-parse --abbrev-ref HEAD").read()
-    branch = branch.strip()
-    s = s + " \033[0;34m[" + branch + "]\033[1;34m"
 print s,
 
 
